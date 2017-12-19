60b2711b88cefab82c82469c2228c2ada9c6a39d
Amr Hesham Gaber <amr96@programmer.net>
2017-12-19 23:04:34 +0200

    Quiz App Tags Class

--- Google Challenge Scholarship/Android Basics/Educational App/app/src/main/java/com/educational/amrdeveloper/educationalapp/MainActivity.java
+++ Google Challenge Scholarship/Android Basics/Educational App/app/src/main/java/com/educational/amrdeveloper/educationalapp/MainActivity.java
@@ -4,14 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
     //User Level Start From Zero
     private int level = 1;
+    //User Score
+    private int userScore = 0;
     //Tags
     private final String LEVEL_TAG = "level";
     //TextView For Every Question
@@ -21,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
     private TextView question4;
     private TextView question5;
     private TextView userLevel;
+    //RadioGroup For Every Question
+    private RadioGroup resultGroup1;
+    private RadioGroup resultGroup2;
+    private RadioGroup resultGroup3;
+    private RadioGroup resultGroup4;
+    private RadioGroup resultGroup5;
     //25 CheckBox
     //Check box for first Question
     private RadioButton result1_1;
@@ -61,10 +71,6 @@ public class MainActivity extends AppCompatActivity {
     private String question_true_Result3;
     private String question_true_Result4;
     private String question_true_Result5;
-    //Full Score
-    private int userScore = 0;
-    //boolean for check if this first time on app or not
-    private boolean isFirst = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,49 +89,52 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null)
         {
             this.level = savedInstanceState.getInt(LEVEL_TAG, 0);
-            this.question1.setText(savedInstanceState.getString("question1"));
-            this.question2.setText(savedInstanceState.getString("question2"));
-            this.question3.setText(savedInstanceState.getString("question3"));
-            this.question4.setText(savedInstanceState.getString("question4"));
-            this.question5.setText(savedInstanceState.getString("question5"));
+            //Update Level TextView
+            userLevel.setText("Level : " + level);
+            //Get The 5 Questions
+            this.question1.setText(savedInstanceState.getString(QuestionTags.QUESTION1));
+            this.question2.setText(savedInstanceState.getString(QuestionTags.QUESTION2));
+            this.question3.setText(savedInstanceState.getString(QuestionTags.QUESTION3));
+            this.question4.setText(savedInstanceState.getString(QuestionTags.QUESTION4));
+            this.question5.setText(savedInstanceState.getString(QuestionTags.QUESTION5));
             //Set Results
             //Question1
-            this.result1_1.setText(savedInstanceState.getString("result1_1"));
-            this.result1_2.setText(savedInstanceState.getString("result1_2"));
-            this.result1_3.setText(savedInstanceState.getString("result1_3"));
-            this.result1_4.setText(savedInstanceState.getString("result1_4"));
+            this.result1_1.setText(savedInstanceState.getString(QuestionTags.RESULT1_1));
+            this.result1_2.setText(savedInstanceState.getString(QuestionTags.RESULT1_2));
+            this.result1_3.setText(savedInstanceState.getString(QuestionTags.RESULT1_3));
+            this.result1_4.setText(savedInstanceState.getString(QuestionTags.RESULT1_4));
             //Question2
-            this.result2_1.setText(savedInstanceState.getString("result2_1"));
-            this.result2_2.setText(savedInstanceState.getString("result2_2"));
-            this.result2_3.setText(savedInstanceState.getString("result2_3"));
-            this.result2_4.setText(savedInstanceState.getString("result2_4"));
+            this.result2_1.setText(savedInstanceState.getString(QuestionTags.RESULT2_1));
+            this.result2_2.setText(savedInstanceState.getString(QuestionTags.RESULT2_2));
+            this.result2_3.setText(savedInstanceState.getString(QuestionTags.RESULT2_3));
+            this.result2_4.setText(savedInstanceState.getString(QuestionTags.RESULT2_4));
             //Question3
-            this.result3_1.setText(savedInstanceState.getString("result3_1"));
-            this.result3_2.setText(savedInstanceState.getString("result3_2"));
-            this.result3_3.setText(savedInstanceState.getString("result3_3"));
-            this.result3_4.setText(savedInstanceState.getString("result3_4"));
+            this.result3_1.setText(savedInstanceState.getString(QuestionTags.RESULT3_1));
+            this.result3_2.setText(savedInstanceState.getString(QuestionTags.RESULT3_2));
+            this.result3_3.setText(savedInstanceState.getString(QuestionTags.RESULT3_3));
+            this.result3_4.setText(savedInstanceState.getString(QuestionTags.RESULT3_4));
             //Question4
-            this.result4_1.setText(savedInstanceState.getString("result4_1"));
-            this.result4_2.setText(savedInstanceState.getString("result4_2"));
-            this.result4_3.setText(savedInstanceState.getString("result4_3"));
-            this.result4_4.setText(savedInstanceState.getString("result4_4"));
+            this.result4_1.setText(savedInstanceState.getString(QuestionTags.RESULT4_1));
+            this.result4_2.setText(savedInstanceState.getString(QuestionTags.RESULT4_2));
+            this.result4_3.setText(savedInstanceState.getString(QuestionTags.RESULT4_3));
+            this.result4_4.setText(savedInstanceState.getString(QuestionTags.RESULT4_4));
             //Question5
-            this.result5_1.setText(savedInstanceState.getString("result5_1"));
-            this.result5_2.setText(savedInstanceState.getString("result5_2"));
-            this.result5_3.setText(savedInstanceState.getString("result5_3"));
-            this.result5_4.setText(savedInstanceState.getString("result5_4"));
+            this.result5_1.setText(savedInstanceState.getString(QuestionTags.RESULT5_1));
+            this.result5_2.setText(savedInstanceState.getString(QuestionTags.RESULT5_2));
+            this.result5_3.setText(savedInstanceState.getString(QuestionTags.RESULT5_3));
+            this.result5_4.setText(savedInstanceState.getString(QuestionTags.RESULT5_4));
             //Set True Result
-            this.question_true_Result1 = savedInstanceState.getString("true_result1");
-            this.question_true_Result2 = savedInstanceState.getString("true_result2");
-            this.question_true_Result3 = savedInstanceState.getString("true_result3");
-            this.question_true_Result4 = savedInstanceState.getString("true_result4");
-            this.question_true_Result5 = savedInstanceState.getString("true_result5");
+            this.question_true_Result1 = savedInstanceState.getString(QuestionTags.TRUE_RESULT1);
+            this.question_true_Result2 = savedInstanceState.getString(QuestionTags.TRUE_RESULT2);
+            this.question_true_Result3 = savedInstanceState.getString(QuestionTags.TRUE_RESULT3);
+            this.question_true_Result4 = savedInstanceState.getString(QuestionTags.TRUE_RESULT4);
+            this.question_true_Result5 = savedInstanceState.getString(QuestionTags.TRUE_RESULT5);
             //Set Current User Result
-            this.question_user_Result1 = savedInstanceState.getString("user_result1");
-            this.question_user_Result2 = savedInstanceState.getString("user_result2");
-            this.question_user_Result3 = savedInstanceState.getString("user_result3");
-            this.question_user_Result4 = savedInstanceState.getString("user_result4");
-            this.question_user_Result5 = savedInstanceState.getString("user_result5");
+            this.question_user_Result1 = savedInstanceState.getString(QuestionTags.USER_RESULT1);
+            this.question_user_Result2 = savedInstanceState.getString(QuestionTags.USER_RESULT2);
+            this.question_user_Result3 = savedInstanceState.getString(QuestionTags.USER_RESULT3);
+            this.question_user_Result4 = savedInstanceState.getString(QuestionTags.USER_RESULT4);
+            this.question_user_Result5 = savedInstanceState.getString(QuestionTags.USER_RESULT5);
         }
         else
         {
@@ -180,28 +189,20 @@ public class MainActivity extends AppCompatActivity {
         this.result5_4 = (RadioButton) findViewById(R.id.result5_4);
     }
 
-    //Un Check All Radio Button
-    private void unCheckAllRadioButtons(){
-        result1_1.setChecked(false);
-        result1_2.setChecked(false);
-        result1_3.setChecked(false);
-        result1_4.setChecked(false);
-        result2_1.setChecked(false);
-        result2_2.setChecked(false);
-        result2_3.setChecked(false);
-        result2_4.setChecked(false);
-        result3_1.setChecked(false);
-        result3_2.setChecked(false);
-        result3_3.setChecked(false);
-        result3_4.setChecked(false);
-        result4_1.setChecked(false);
-        result4_2.setChecked(false);
-        result4_3.setChecked(false);
-        result4_4.setChecked(false);
-        result5_1.setChecked(false);
-        result5_2.setChecked(false);
-        result5_3.setChecked(false);
-        result5_4.setChecked(false);
+    //un Check all Radio Buttons Using Radio Groups
+    private void unCheckAllRadioGroups(){
+        //initialization Five Radio Group one for every Question
+        resultGroup1 = (RadioGroup)findViewById(R.id.resultGroup1);
+        resultGroup2 = (RadioGroup)findViewById(R.id.resultGroup2);
+        resultGroup3 = (RadioGroup)findViewById(R.id.resultGroup3);
+        resultGroup4 = (RadioGroup)findViewById(R.id.resultGroup4);
+        resultGroup5 = (RadioGroup)findViewById(R.id.resultGroup5);
+        //Clear Check for all Gruoup
+        resultGroup1.clearCheck();
+        resultGroup2.clearCheck();
+        resultGroup3.clearCheck();
+        resultGroup4.clearCheck();
+        resultGroup5.clearCheck();
     }
 
     //Question 1 set title and results
@@ -288,29 +289,36 @@ public class MainActivity extends AppCompatActivity {
             userScore = userScore + 1;
     }
 
-    //Submit The Result And Show Score and if score is full go next round
-    public void sumbitResultButton(View v) {
+    //Submit The Result And Show Score and if score is full go next Level
+    public void submitResultButton(View v) {
         //Submit Result
         setUserScore();
         //If Score Is Full Go Next Level
         if (this.userScore == 5) {
+            //Change Question To New Questions
             setQuestion1();
             setQuestion2();
             setQuestion3();
             setQuestion4();
             setQuestion5();
+            //Go Next Level
             level++;
             userLevel.setText("Level : " + level);
+            //Reset User Score to Zero
             this.userScore = 0;
-            unCheckAllRadioButtons();
-            Toast.makeText(this, "Full Score \n Go to Next Level now : D", Toast.LENGTH_SHORT).show();
+            //un Check All Radio Buttons
+            unCheckAllRadioGroups();
+            //Print Full Score Message
+            Toast.makeText(this, getResources().getString(R.string.full_scaore), Toast.LENGTH_SHORT).show();
         }
-        else{
-            Toast.makeText(this, "Sorry You Need Full Score to Go Next Level\n ~_~", Toast.LENGTH_SHORT).show();
+        else
+        {
+            //Print Current Score and Stay in Current Level
+            Toast.makeText(this, getResources().getString(R.string.not_full_score) + userScore, Toast.LENGTH_SHORT).show();
         }
     }
 
-    //Get Qustion 1 result
+    //Get Question 1 user result
     public void onQuestionResult1(View view) {
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -334,7 +342,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Get Qustion 2 result
+    //Get Question 2 user result
     public void onQuestionResult2(View view) {
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -358,7 +366,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Get Qustion 3 result
+    //Get Question 3 user result
     public void onQuestionResult3(View view) {
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -382,7 +390,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Get Qustion 4 result
+    //Get Question 4 user result
     public void onQuestionResult4(View view) {
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -406,7 +414,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Get Qustion 5 result
+    //Get Question 5 user result
     public void onQuestionResult5(View view) {
         boolean checked = ((RadioButton) view).isChecked();
         // Check which radio button was clicked
@@ -437,48 +445,48 @@ public class MainActivity extends AppCompatActivity {
         //Save Current level
         outState.putInt(LEVEL_TAG, level);
         //Save Every Question
-        outState.putString("question1",question1.getText().toString());
-        outState.putString("question2",question2.getText().toString());
-        outState.putString("question3",question3.getText().toString());
-        outState.putString("question4",question4.getText().toString());
-        outState.putString("question5",question5.getText().toString());
+        outState.putString(QuestionTags.QUESTION1 , question1.getText().toString());
+        outState.putString(QuestionTags.QUESTION2 , question2.getText().toString());
+        outState.putString(QuestionTags.QUESTION3 , question3.getText().toString());
+        outState.putString(QuestionTags.QUESTION4 , question4.getText().toString());
+        outState.putString(QuestionTags.QUESTION5 , question5.getText().toString());
         //Save Result For Every Question
         //Question1
-        outState.putString("result1_1",result1_1.getText().toString());
-        outState.putString("result1_2",result1_2.getText().toString());
-        outState.putString("result1_3",result1_3.getText().toString());
-        outState.putString("result1_4",result1_4.getText().toString());
+        outState.putString(QuestionTags.RESULT1_1 , result1_1.getText().toString());
+        outState.putString(QuestionTags.RESULT1_2 , result1_2.getText().toString());
+        outState.putString(QuestionTags.RESULT1_3 , result1_3.getText().toString());
+        outState.putString(QuestionTags.RESULT1_4 ,result1_4.getText().toString());
         //Question2
-        outState.putString("result2_1",result2_1.getText().toString());
-        outState.putString("result2_2",result2_2.getText().toString());
-        outState.putString("result2_3",result2_3.getText().toString());
-        outState.putString("result2_4",result2_4.getText().toString());
+        outState.putString(QuestionTags.RESULT2_1 , result2_1.getText().toString());
+        outState.putString(QuestionTags.RESULT2_2 , result2_2.getText().toString());
+        outState.putString(QuestionTags.RESULT2_3 , result2_3.getText().toString());
+        outState.putString(QuestionTags.RESULT2_4 , result2_4.getText().toString());
         //Question3
-        outState.putString("result3_1",result3_1.getText().toString());
-        outState.putString("result3_2",result3_2.getText().toString());
-        outState.putString("result3_3",result3_3.getText().toString());
-        outState.putString("result3_4",result3_4.getText().toString());
+        outState.putString(QuestionTags.RESULT3_1 , result3_1.getText().toString());
+        outState.putString(QuestionTags.RESULT3_2 , result3_2.getText().toString());
+        outState.putString(QuestionTags.RESULT3_3 , result3_3.getText().toString());
+        outState.putString(QuestionTags.RESULT3_4 , result3_4.getText().toString());
         //Question4
-        outState.putString("result4_1",result4_1.getText().toString());
-        outState.putString("result4_2",result4_2.getText().toString());
-        outState.putString("result4_3",result4_3.getText().toString());
-        outState.putString("result4_4",result4_4.getText().toString());
-        //Question4
-        outState.putString("result5_1",result5_1.getText().toString());
-        outState.putString("result5_2",result5_2.getText().toString());
-        outState.putString("result5_3",result5_3.getText().toString());
-        outState.putString("result5_4",result5_4.getText().toString());
+        outState.putString(QuestionTags.RESULT4_1 , result4_1.getText().toString());
+        outState.putString(QuestionTags.RESULT4_2 , result4_2.getText().toString());
+        outState.putString(QuestionTags.RESULT4_3 , result4_3.getText().toString());
+        outState.putString(QuestionTags.RESULT4_4 , result4_4.getText().toString());
+        //Question5
+        outState.putString(QuestionTags.RESULT5_1 , result5_1.getText().toString());
+        outState.putString(QuestionTags.RESULT5_2 , result5_2.getText().toString());
+        outState.putString(QuestionTags.RESULT5_3 , result5_3.getText().toString());
+        outState.putString(QuestionTags.RESULT5_4 , result5_4.getText().toString());
         //Save True Result
-        outState.putString("true_result1",question_true_Result1);
-        outState.putString("true_result2",question_true_Result2);
-        outState.putString("true_result3",question_true_Result3);
-        outState.putString("true_result4",question_true_Result4);
-        outState.putString("true_result5",question_true_Result5);
+        outState.putString(QuestionTags.TRUE_RESULT1 , question_true_Result1);
+        outState.putString(QuestionTags.TRUE_RESULT2 , question_true_Result2);
+        outState.putString(QuestionTags.TRUE_RESULT3 , question_true_Result3);
+        outState.putString(QuestionTags.TRUE_RESULT4 , question_true_Result4);
+        outState.putString(QuestionTags.TRUE_RESULT5 , question_true_Result5);
         //Set User Current Result
-        outState.putString("user_result1",question_user_Result1);
-        outState.putString("user_result2",question_user_Result2);
-        outState.putString("user_result3",question_user_Result3);
-        outState.putString("user_result4",question_user_Result4);
-        outState.putString("user_result5",question_user_Result5);
+        outState.putString(QuestionTags.USER_RESULT1 , question_user_Result1);
+        outState.putString(QuestionTags.USER_RESULT2 , question_user_Result2);
+        outState.putString(QuestionTags.USER_RESULT3 , question_user_Result3);
+        outState.putString(QuestionTags.USER_RESULT4 , question_user_Result4);
+        outState.putString(QuestionTags.USER_RESULT5 , question_user_Result5);
     }
 }
---
+++ Google Challenge Scholarship/Android Basics/Educational App/app/src/main/java/com/educational/amrdeveloper/educationalapp/QuestionTags.java
@@ -0,0 +1,62 @@
+package com.educational.amrdeveloper.educationalapp;
+
+/**
+ * Created by AmrDeveloper on 12/19/2017.
+ */
+
+public class QuestionTags {
+    //Class For Every Question and Result Tag
+    //Question Title 1 - 5
+    protected static final String QUESTION1 = "question1";
+    protected static final String QUESTION2 = "question2";
+    protected static final String QUESTION3 = "question3";
+    protected static final String QUESTION4 = "question4";
+    protected static final String QUESTION5 = "question5";
+
+    //5 Result for every Question
+    //Question Number one
+    protected static final String RESULT1_1 = "result1_1";
+    protected static final String RESULT1_2 = "result1_2";
+    protected static final String RESULT1_3 = "result1_3";
+    protected static final String RESULT1_4 = "result1_4";
+
+    //Question Number Two
+    protected static final String RESULT2_1 = "result2_1";
+    protected static final String RESULT2_2 = "result2_2";
+    protected static final String RESULT2_3 = "result2_3";
+    protected static final String RESULT2_4 = "result2_4";
+
+    //Question Number Three
+    protected static final String RESULT3_1 = "result3_1";
+    protected static final String RESULT3_2 = "result3_2";
+    protected static final String RESULT3_3 = "result3_3";
+    protected static final String RESULT3_4 = "result3_4";
+
+    //Question Number Four
+    protected static final String RESULT4_1 = "result4_1";
+    protected static final String RESULT4_2 = "result4_2";
+    protected static final String RESULT4_3 = "result4_3";
+    protected static final String RESULT4_4 = "result4_4";
+
+    //Question Number Five
+    protected static final String RESULT5_1 = "result5_1";
+    protected static final String RESULT5_2 = "result5_2";
+    protected static final String RESULT5_3 = "result5_3";
+    protected static final String RESULT5_4 = "result5_4";
+
+    //The True Result Tag
+    protected static final String TRUE_RESULT1 = "true_result1";
+    protected static final String TRUE_RESULT2 = "true_result2";
+    protected static final String TRUE_RESULT3 = "true_result3";
+    protected static final String TRUE_RESULT4 = "true_result4";
+    protected static final String TRUE_RESULT5 = "true_result5";
+
+    //The User Result Tag
+    protected static final String USER_RESULT1 = "user_result1";
+    protected static final String USER_RESULT2 = "user_result2";
+    protected static final String USER_RESULT3 = "user_result3";
+    protected static final String USER_RESULT4 = "user_result4";
+    protected static final String USER_RESULT5 = "user_result5";
+
+
+}
