18a8f262ac2e4f5a8b5eefba0a12d5d4c9e7bbcb
AmrDeveloper <amr96@programmer.net>
2018-07-12 16:51:49 +0200

    Tweet app to implement Twitter SDK 3.3.1

---
+++ Side Projects/Tweet/app/src/main/java/amrdeveloper/com/tweet/LoginActivity.java
@@ -0,0 +1,60 @@
+package amrdeveloper.com.tweet;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TwitterLoginButton twitterLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Twitter.initialize(this);
+        setContentView(R.layout.activity_login);
+
+        twitterLoginButton = findViewById(R.id.twitterLoginButton);
+        twitterLoginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+
+                userLogin(session);
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+                Toast.makeText(LoginActivity.this, "Invalid", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void userLogin(TwitterSession session){
+         String username = session.getUserName();
+         Intent intent = new Intent(this,MainActivity.class);
+         intent.putExtra("username",username);
+         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Pass the activity result to the login button.
+        twitterLoginButton.onActivityResult(requestCode, resultCode, data);
+    }
+}
