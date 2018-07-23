2bceb6eb5f141b10e91a3a529ebd7bfc1124e508
AmrDeveloper <amr96@programmer.net>
2018-07-24 00:15:23 +0200

    Update news app

---
+++ Side Projects/World News v3/app/src/main/java/com/worldnews/amrdeveloper/worldnews/activities/WebViewerActivity.java
@@ -0,0 +1,24 @@
+package com.worldnews.amrdeveloper.worldnews.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.worldnews.amrdeveloper.worldnews.R;
+
+public class WebViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_viewr);
+
+        Intent intent = getIntent();
+        String url = intent.getStringExtra("newsUrl");
+        WebView web = (WebView) findViewById(R.id.webView);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl(url);
+    }
+}
