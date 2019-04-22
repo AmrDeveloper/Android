b5943b944f3f2bfe5d599e8ed6986bbd9e0a5907
AmrDeveloper <Amr96@programmer.net>
2019-04-23 01:52:55 +0200

    [Hangman] get view and print char when user click on gridview item

--- Side Projects/Hangman/app/src/main/java/com/amrdeveloper/hangman/MainActivity.java
+++ Side Projects/Hangman/app/src/main/java/com/amrdeveloper/hangman/MainActivity.java
@@ -2,13 +2,24 @@ package com.amrdeveloper.hangman;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView attemptsNumberTxt;
+    private TextView hangmanWordTxt;
     private GridView charsGridView;
+
+    private CharactersGridAdapter adapter;
+
+    private static final String INVALID_NUMBER_FORMAT = "Attempts Number : %d";
     private static final int INVALID_ANSWERS_NUMBER = 5;
     private int currentInvalidAnswers = INVALID_ANSWERS_NUMBER;
 
@@ -16,10 +27,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
 
-        charsGridView = findViewById(R.id.charsGridView);
         List<String> alphabetChars = AlphabetGenerator.getAlphabetChars();
-        CharactersGridAdapter adapter = new CharactersGridAdapter(this,alphabetChars);
+        adapter = new CharactersGridAdapter(this, alphabetChars);
         charsGridView.setAdapter(adapter);
+        charsGridView.setOnItemClickListener(onCharItemClickListener);
+
+        attemptsNumberTxt.setText(String.format(Locale.ENGLISH, INVALID_NUMBER_FORMAT, currentInvalidAnswers));
+        hangmanWordTxt.setText("******");
     }
+
+    private void initViews() {
+        charsGridView = findViewById(R.id.charsGridView);
+        attemptsNumberTxt = findViewById(R.id.attemptsNumberTxt);
+        hangmanWordTxt = findViewById(R.id.hangmanWordTxt);
+    }
+
+    private AdapterView.OnItemClickListener onCharItemClickListener = (parent, view, position, id) -> {
+        View currentChar = adapter.getView(position, view, parent);
+        Toast.makeText(this, "Char : " + adapter.getItem(position), Toast.LENGTH_SHORT).show();
+    };
 }
