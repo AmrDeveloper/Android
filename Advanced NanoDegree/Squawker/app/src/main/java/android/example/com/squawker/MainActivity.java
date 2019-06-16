770469d63274b4a3f3609780d7e48f72bb31fd9c
AmrDeveloper <Amr96@programmer.net>
2019-06-16 16:39:16 +0200

    [Squawker]add firebase squawker app

---
+++ Advanced NanoDegree/Squawker/app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -0,0 +1,147 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package android.example.com.squawker;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.example.com.squawker.following.FollowingPreferenceActivity;
+import android.example.com.squawker.provider.SquawkContract;
+import android.example.com.squawker.provider.SquawkProvider;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
+import androidx.preference.PreferenceManager;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int LOADER_ID_MESSAGES = 0;
+
+    RecyclerView mRecyclerView;
+    LinearLayoutManager mLayoutManager;
+    SquawkAdapter mAdapter;
+
+    static final String[] MESSAGES_PROJECTION = {
+            SquawkContract.COLUMN_AUTHOR,
+            SquawkContract.COLUMN_MESSAGE,
+            SquawkContract.COLUMN_DATE,
+            SquawkContract.COLUMN_AUTHOR_KEY
+    };
+
+    static final int COL_NUM_AUTHOR = 0;
+    static final int COL_NUM_MESSAGE = 1;
+    static final int COL_NUM_DATE = 2;
+    static final int COL_NUM_AUTHOR_KEY = 3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.squawks_recycler_view);
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // Use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Add dividers
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(
+                mRecyclerView.getContext(),
+                mLayoutManager.getOrientation());
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
+        // Specify an adapter
+        mAdapter = new SquawkAdapter();
+        mRecyclerView.setAdapter(mAdapter);
+
+        // Start the loader
+        LoaderManager.getInstance(this).initLoader(LOADER_ID_MESSAGES, null, this);
+
+        // Get token from the ID Service you created and show it in a log
+        FirebaseInstanceId.getInstance().getInstanceId().addOnSuccessListener(instanceIdResult -> {
+            String token = instanceIdResult.getToken();
+            String msg = getString(R.string.message_token_format, token);
+            Log.d(LOG_TAG, msg);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_following_preferences) {
+            // Opens the following activity when the menu icon is pressed
+            Intent startFollowingActivity = new Intent(this, FollowingPreferenceActivity.class);
+            startActivity(startFollowingActivity);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Loader callbacks
+     */
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // This method generates a selection off of only the current followers
+        String selection = SquawkContract.createSelectionForCurrentFollowers(
+                PreferenceManager.getDefaultSharedPreferences(this));
+        Log.d(LOG_TAG, "Selection is " + selection);
+        return new CursorLoader(this, SquawkProvider.SquawkMessages.CONTENT_URI,
+                MESSAGES_PROJECTION, selection, null, SquawkContract.COLUMN_DATE + " DESC");
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+}
---
+++ Advanced NanoDegree/Squawker/app/src/main/java/android/example/com/squawker/following/FollowingPreferenceFragment.java
@@ -0,0 +1,91 @@
+/*
+* Copyright (C) 2017 The Android Open Source Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*  	http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package android.example.com.squawker.following;
+
+import android.content.SharedPreferences;
+import android.example.com.squawker.R;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+
+
+/**
+ * Shows the list of instructors you can follow
+ */
+public class FollowingPreferenceFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private final static String LOG_TAG = FollowingPreferenceFragment.class.getSimpleName();
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Add visualizer preferences, defined in the XML file in res->xml->preferences_squawker
+        addPreferencesFromResource(R.xml.following_squawker);
+    }
+
+    /**
+     * Triggered when shared preferences changes. This will be triggered when a person is followed
+     * or un-followed
+     *
+     * @param sharedPreferences SharedPreferences file
+     * @param key               The key of the preference which was changed
+     */
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        Preference preference = findPreference(key);
+        if (preference != null && preference instanceof SwitchPreferenceCompat) {
+            // Get the current state of the switch preference
+            boolean isOn = sharedPreferences.getBoolean(key, false);
+            if (isOn) {
+                // The preference key matches the following key for the associated instructor in
+                // FCM. For example, the key for Lyla is key_lyla (as seen in
+                // following_squawker.xml). The topic for Lyla's messages is /topics/key_lyla
+
+                // Subscribe
+                FirebaseMessaging.getInstance().subscribeToTopic(key);
+                Log.d(LOG_TAG, "Subscribing to " + key);
+            } else {
+                // Un-subscribe
+                FirebaseMessaging.getInstance().unsubscribeFromTopic(key);
+                Log.d(LOG_TAG, "Un-subscribing to " + key);
+            }
+        }
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Add the shared preference change listener
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Remove the shared preference change listener
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+}
